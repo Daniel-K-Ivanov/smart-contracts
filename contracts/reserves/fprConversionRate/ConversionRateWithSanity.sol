e872497825e4268dd818e16957eff24a9cc287d0
Ilan Doron <ilan.on.the.road@gmail.com>
2020-02-20 05:01:33 +0200

    conversion rate with get sanity rate

---
+++ contracts/reserves/fprConversionRate/ConversionRateWithSanity.sol
@@ -0,0 +1,47 @@
+pragma solidity 0.4.18;
+
+
+import "./ConversionRates.sol";
+
+
+contract ConversionRatesWithSanity is ConversionRates {
+
+    function getSanityRate(ERC20 token, bool buy) public view returns(uint) {
+        if (!tokenData[token].enabled) return 0;
+        if (tokenControlInfo[token].minimalRecordResolution == 0) return 0; // token control info not set
+
+        // get rate update block
+        bytes32 compactData = tokenRatesCompactData[tokenData[token].compactDataArrayIndex];
+
+        uint updateRateBlock = getLast4Bytes(compactData);
+        if (block.number >= updateRateBlock + validRateDurationInBlocks) return 0; // rate expired
+        // check imbalance
+        
+        // calculate actual rate
+        int extraBps;
+        int8 rateUpdate;
+        uint rate;
+
+        if (buy) {
+            // start with base rate
+            rate = tokenData[token].baseBuyRate;
+
+            // add rate update
+            rateUpdate = getRateByteFromCompactData(compactData, token, true);
+            extraBps = int(rateUpdate) * 10;
+            rate = addBps(rate, extraBps);
+
+        } else {
+            // start with base rate
+            rate = tokenData[token].baseSellRate;
+
+            // add rate update
+            rateUpdate = getRateByteFromCompactData(compactData, token, false);
+            extraBps = int(rateUpdate) * 10;
+            rate = addBps(rate, extraBps);
+
+        }
+
+        return rate;    
+    }
+}
